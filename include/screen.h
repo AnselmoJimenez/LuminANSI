43bc19f928655f0dcd89d142de8fbbb927c88268
Anselmo <anselmojimenez0@gmail.com>
2025-07-09 21:41:46 -0400

    TODO: fix plot

--- include/screen.h
+++ include/screen.h
@@ -1,2 +1,8 @@
 // init_window : initialize a window of size height and width
 int init_window(const int height, const int width);
+
+// draw_window : draw the buffer of characters
+int draw_window(void);
+
+// plot : plot a pixel onto the window
+void plot(vertex_t);
