913ab591622d55601227df3f5be75d6665a959ab
Anselmo <anselmojimenez0@gmail.com>
2025-07-09 21:42:33 -0400

    TODO: Implement projection

--- source/screen.c
+++ source/screen.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include "../include/polygon.h"
 #include "../include/screen.h"
 
 typedef struct {
@@ -9,7 +10,23 @@ typedef struct {
     int width;
     char **pixels;
 } window_t;
+
 window_t window;
+
+#define PATTERNSIZE 4
+const char *pattern[] = {
+    " ",             // empty space
+    "\xe2\xa1\x80",  // ⡀
+    "\xe2\xa3\x80",  // ⣀
+    "\xe2\xa3\x84",  // ⣄
+    "\xe2\xa3\xa4",  // ⣤
+    "\xe2\xa3\xa6",  // ⣦
+    "\xe2\xa3\xb6",  // ⣶
+    "\xe2\xa3\xb7",  // ⣷
+    "\xe2\xa3\xbf",  // ⣿
+    "\xe2\x96\x88"   // █
+};
+
 
 // init_window : initialize a window of size height and width
 int init_window(const int height, const int width) {
@@ -19,7 +36,26 @@ int init_window(const int height, const int width) {
     if (window.pixels == NULL)
         return 1;
     
-    char **pxb = window.pixels;
-    while (window.pixels + (window.height * window.width) > pxb)
-        *pxb++ = " ";
+    for (int i = 0; i < window.height * window.width; i++) {
+        window.pixels[i] = malloc(PATTERNSIZE * sizeof(char));  // Space + null terminator
+        strcpy(window.pixels[i], pattern[8]);
+    }
+
+    return 0;
+}
+
+// draw_window : draw the buffer of characters
+int draw_window(void) {
+    for (int i = 0; i < window.height * window.width; i++) {
+        if (i % window.width == 0) printf("\n%s", window.pixels[i]);
+        else printf("%s", window.pixels[i]);
+    }
+    printf("\n");
+}
+
+// plot : plot a pixel onto the window
+void plot(vertex_t v) {
+    int x_offset = v.x % window.width;
+    int y_offset = v.y % window.height;
+    window.pixels[y_offset * x_offset] = v.pattern;
 }
