6e92833764d8a87807eaa5270b5bdb0aebfb600b
Anselmo <anselmojimenez0@gmail.com>
2025-07-09 21:42:03 -0400

    Testing plotting

--- source/main.c
+++ source/main.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 
+#include "../include/polygon.h"
 #include "../include/screen.h"
 
 int main(int argc, char const *argv[]) {
-    const int height = 420;
-    const int width = 840;
+    const int height = 15; // 128
+    const int width = 30;  // 512
 
-    init_window(height, width);
+    if (init_window(height, width) != 0)
+        return 1;
 
-    // draw_screen(screen, window);
+    draw_window();
 
+    vertex_t myv = {
+        .x = 15,
+        .y = 5,
+        .z = 0,
+        .pattern = "\xe2\x96\x88"   // █
+    };
+
+    plot(myv);
+    draw_window();
     return 0;
 }
