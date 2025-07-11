39878736a087cbe251b1f053486a468e42865334
Anselmo Jimenez <anselmojimenez0@gmail.com>
2025-07-10 22:07:25 -0400

    implemented rotation functions and vertex constructor

--- include/polygon.h
+++ include/polygon.h
@@ -1,6 +1,18 @@
 typedef struct {
-    int x;
-    int y;
-    int z;
+    double x;
+    double y;
+    double z;
     char *pattern;
 } vertex_t;
+
+// new_vertex : create and return a new vertex object
+vertex_t new_vertex(double, double, double);
+
+// rotate_x : rotate the vertex coordinate by angle in the x direction
+void rotate_x(double, vertex_t *);
+
+// rotate_y : rotate the vertex coordinate by angle in the y direction
+void rotate_y(double, vertex_t *);
+
+// rotate_z : rotate the vertex coordinate by angle in the z direction
+void rotate_z(double, vertex_t *);
---
+++ source/polygon.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <math.h>
+
+#include "../include/polygon.h"
+
+// new_vertex : create and return a new vertex object
+vertex_t new_vertex(double x, double y, double z) {
+    vertex_t v = {
+        .x = x,
+        .y = y,
+        .z = z,
+        .pattern = "\xe2\x96\x88"   // █
+    };
+    return v;
+}
+
+// rotate_x : rotate the vertex coordinate by angle in the x direction
+void rotate_x(double angle, vertex_t *v) {
+    double y1 = v->y;
+    double z1 = v->z;
+    v->y = y1 * cos(angle) - z1 * sin(angle);
+    v->z = z1 * sin(angle) + z1 * cos(angle);
+}
+
+// rotate_y : rotate the vertex coordinate by angle in the y direction
+void rotate_y(double angle, vertex_t *v) {
+    double x1 = v->x;
+    double z1 = v->z;
+    v->x = x1 * cos(angle) + z1 * sin(angle);
+    v->z = -x1 * sin(angle) + z1 * cos(angle);
+}
+
+// rotate_z : rotate the vertex coordinate by angle in the z direction
+void rotate_z(double angle, vertex_t *v) {
+    double x1 = v->x;
+    double y1 = v->y;
+    v->x = x1 * cos(angle) - y1 * sin(angle);
+    v->y = x1 * sin(angle) + y1 * cos(angle);
+}
