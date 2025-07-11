e337d5dc356d529885709a7f7c87e0d92d6557a0
Anselmo Jimenez <anselmojimenez0@gmail.com>
2025-07-10 22:08:17 -0400

    implemented destructor for screen and plot function

--- include/screen.h
+++ include/screen.h
@@ -1,8 +1,11 @@
 // init_window : initialize a window of size height and width
 int init_window(const int height, const int width);
 
+// destroy_window: free the memory contained in the window
+void destroy_window(void);
+
 // draw_window : draw the buffer of characters
 int draw_window(void);
 
-// plot : plot a pixel onto the window
+// plot : put vertex in screen buffer
 void plot(vertex_t);
--- source/screen.c
+++ source/screen.c
@@ -38,24 +38,42 @@ int init_window(const int height, const int width) {
     
     for (int i = 0; i < window.height * window.width; i++) {
         window.pixels[i] = malloc(PATTERNSIZE * sizeof(char));  // Space + null terminator
-        strcpy(window.pixels[i], pattern[8]);
+        strcpy(window.pixels[i], pattern[1]);
     }
 
     return 0;
 }
 
+// destroy_window: free the memory contained in the window
+void destroy_window(void) {
+    for (int i = 0; i < window.height * window.width; i++)
+        free(window.pixels[i]);
+    free(window.pixels);
+}
+
 // draw_window : draw the buffer of characters
 int draw_window(void) {
     for (int i = 0; i < window.height * window.width; i++) {
-        if (i % window.width == 0) printf("\n%s", window.pixels[i]);
-        else printf("%s", window.pixels[i]);
+        if (i % window.width == 0 && i != 0) printf("\n");
+        printf("%s", window.pixels[i]);
     }
     printf("\n");
 }
 
-// plot : plot a pixel onto the window
+// plot : put vertex in pixels buffer
 void plot(vertex_t v) {
-    int x_offset = v.x % window.width;
-    int y_offset = v.y % window.height;
-    window.pixels[y_offset * x_offset] = v.pattern;
+    double d = 1; // distance of image plane (screen)
+
+    // projected coordinated on a the image plane
+    double projx = (v.x * d) / v.z;
+    double projy = (v.y * d) / v.z;
+
+    // convert to pixel coordinates
+    double pxx = (window.width / 2) + projx;
+    double pxy = (window.height / 2) + projy;
+
+    // array index
+    int index = (window.width * (int) pxy) + (int) pxx;
+    if (index >= 0 && index < window.height * window.width) // quick bounds check
+        strcpy(window.pixels[index], v.pattern);
 }
