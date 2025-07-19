81c53b75b7d321bd3840033db215367990c67ac4
Anselmo <anselmojimenez0@gmail.com>
2025-07-18 23:15:48 -0400

    testing vertices and rotation

--- source/main.c
+++ source/main.c
@@ -3,16 +3,34 @@
 #include "../include/polygon.h"
 #include "../include/screen.h"
 
+#define MAXVERTICES 50
+
+vertex_t vertices[MAXVERTICES] = {0};
+vertex_t *vp = vertices;
+
 int main(int argc, char const *argv[]) {
-    const int height = 128;
-    const int width = 512;
+    const int height = 64;
+    const int width = 256;
 
     if (init_window(height, width) != 0)
         return 1;
 
-    vertex_t myv = new_vertex(0, 0, 10);
-    plot(myv);
+    *vp++ = new_vertex( 1,  1, 10);
+    *vp++ = new_vertex(-1, -1, 10);
+    *vp++ = new_vertex( 1, -1, 10);
+    *vp++ = new_vertex(-1,  1, 10);
+    for (int i = 0; i < 4; i++)
+        plot(vertices[i]);
     draw_window();
+
+    for (int timer = 0; timer < 50; timer++) {
+        clear_window();
+        for (int i = 0; i < 4; i++) {
+            rotate_z(1, &vertices[i]);
+            plot(vertices[i]);
+        }
+        draw_window();
+    }
 
     destroy_window();
     return 0;
