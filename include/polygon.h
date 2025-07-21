a5f9429358d323a7af2d43b015434f9189c6b9b0
Anselmo <anselmojimenez0@gmail.com>
2025-07-21 18:05:50 -0400

    Troubleshooting connection

--- include/polygon.h
+++ include/polygon.h
@@ -9,10 +9,10 @@ typedef struct {
 vertex_t new_vertex(double, double, double);
 
 // rotate_x : rotate the vertex coordinate by angle in the x direction
-void rotate_x(double, vertex_t *);
+void rotate_x(double, vertex_t *, vertex_t);
 
 // rotate_y : rotate the vertex coordinate by angle in the y direction
-void rotate_y(double, vertex_t *);
+void rotate_y(double, vertex_t *, vertex_t);
 
 // rotate_z : rotate the vertex coordinate by angle in the z direction
-void rotate_z(double, vertex_t *);
+void rotate_z(double, vertex_t *, vertex_t);
--- source/polygon.c
+++ source/polygon.c
@@ -2,6 +2,7 @@
 #include <math.h>
 
 #include "../include/polygon.h"
+#include "../include/screen.h"
 
 // new_vertex : create and return a new vertex object
 vertex_t new_vertex(double x, double y, double z) {
@@ -11,27 +12,28 @@ vertex_t new_vertex(double x, double y, double z) {
         .z = z,
         .pattern = "\xe2\x96\x88"   // █
     };
+
     return v;
 }
 
-// rotate_x : rotate the vertex coordinate by angle in the x direction
-void rotate_x(double angle, vertex_t *v) {
+// rotate_x : rotate the vertex coordinate by angle in the x direction about c
+void rotate_x(double angle, vertex_t *v, vertex_t c) {
     double y1 = v->y;
-    double z1 = v->z;
+    double z1 = v->z - c.z;
     v->y = y1 * cos(angle) - z1 * sin(angle);
-    v->z = y1 * sin(angle) + z1 * cos(angle);
+    v->z = y1 * sin(angle) + z1 * cos(angle) + c.z;
 }
 
-// rotate_y : rotate the vertex coordinate by angle in the y direction
-void rotate_y(double angle, vertex_t *v) {
+// rotate_y : rotate the vertex coordinate by angle in the y direction about c
+void rotate_y(double angle, vertex_t *v, vertex_t c) {
     double x1 = v->x;
-    double z1 = v->z;
+    double z1 = v->z - c.z;
     v->x = x1 * cos(angle) + z1 * sin(angle);
-    v->z = -x1 * sin(angle) + z1 * cos(angle);
+    v->z = -x1 * sin(angle) + z1 * cos(angle) + c.z;
 }
 
-// rotate_z : rotate the vertex coordinate by angle in the z direction
-void rotate_z(double angle, vertex_t *v) {
+// rotate_z : rotate the vertex coordinate by angle in the z direction about c
+void rotate_z(double angle, vertex_t *v, vertex_t c) {
     double x1 = v->x;
     double y1 = v->y;
     v->x = x1 * cos(angle) - y1 * sin(angle);
