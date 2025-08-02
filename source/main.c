1897f8436e610cfd2b7379de66f69b9bd84af7b9
Anselmo Jimenez <anselmojimenez0@gmail.com>
2025-08-02 12:03:20 -0400

    testing rotations and vertices

--- source/main.c
+++ source/main.c
@@ -6,41 +6,52 @@
 #define MAXVERTICES 50
 
 vertex_t vertices[MAXVERTICES];
-vertex_t *vp = vertices;
+vertex_t transforms[MAXVERTICES];
 
 int main(int argc, char const *argv[]) {
     const int height = 64;
     const int width = 256;
+    double rotation_angle = 0;
 
     if (init_window(height, width) != 0)
         return 1;
 
     vertex_t center = new_vertex(0, 0, 11);
-    *vp++ = new_vertex( 1,  1, 10);
-    *vp++ = new_vertex(-1, -1, 10);
-    *vp++ = new_vertex( 1, -1, 10);
-    *vp++ = new_vertex(-1,  1, 10);
-    // *vp++ = new_vertex( 1,  1, 12);
-    // *vp++ = new_vertex(-1, -1, 12);
-    // *vp++ = new_vertex( 1, -1, 12);
-    // *vp++ = new_vertex(-1,  1, 12);
-    
-    for (int i = 0; i < 4; i++)
-        plot(vertices[i]);
+    vertices[0] = new_vertex( 1,  1, 10);
+    vertices[1] = new_vertex(-1, -1, 10);
+    vertices[2] = new_vertex( 1, -1, 10);
+    vertices[3] = new_vertex(-1,  1, 10);
+    vertices[4] = new_vertex( 1,  1, 12);
+    vertices[5] = new_vertex(-1, -1, 12);
+    vertices[6] = new_vertex( 1, -1, 12);
+    vertices[7] = new_vertex(-1,  1, 12);
 
-    for (int count = 0; count < 2000; count++) {
+    for (int count = 0; count < 4000; count++) {
         clear_window();
-        for (int i = 0; i < 4; i++) {
-            rotate_y(0.0025, &vertices[i], center);
+
+        rotation_angle += 0.0025;
+
+        for (int i = 0; i < 8; i++) {
+            transforms[i] = vertices[i];
+            rotate_y(rotation_angle, &transforms[i], center);
+            rotate_x(rotation_angle, &transforms[i], center);
+            plot(transforms[i]);
         }
-        connect(vertices[1], vertices[2]);
-        connect(vertices[2], vertices[3]);
-        connect(vertices[3], vertices[0]);
-        connect(vertices[0], vertices[2]);
-        connect(vertices[1], vertices[3]);
+        connect(transforms[0], transforms[2]);
+        connect(transforms[0], transforms[3]);
+        connect(transforms[0], transforms[4]);
+        connect(transforms[1], transforms[2]);
+        connect(transforms[1], transforms[3]);
+        connect(transforms[1], transforms[5]);
+        connect(transforms[2], transforms[6]);
+        connect(transforms[3], transforms[7]);
+        connect(transforms[4], transforms[6]);
+        connect(transforms[4], transforms[7]);
+        connect(transforms[5], transforms[6]);
+        connect(transforms[5], transforms[7]);
+
         draw_window();
     }
-
     destroy_window();
     return 0;
 }
