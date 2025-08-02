c0fa39bcf4d3049f7c4c370a41a6564946a83aba
Anselmo Jimenez <anselmojimenez0@gmail.com>
2025-08-02 12:03:55 -0400

    Corrected rotation logic

--- source/polygon.c
+++ source/polygon.c
@@ -18,24 +18,24 @@ vertex_t new_vertex(double x, double y, double z) {
 
 // rotate_x : rotate the vertex coordinate by angle in the x direction about c
 void rotate_x(double angle, vertex_t *v, vertex_t c) {
-    double y1 = v->y;
+    double y1 = v->y - c.y;
     double z1 = v->z - c.z;
-    v->y = y1 * cos(angle) - z1 * sin(angle);
+    v->y = y1 * cos(angle) - z1 * sin(angle) + c.y;
     v->z = y1 * sin(angle) + z1 * cos(angle) + c.z;
 }
 
 // rotate_y : rotate the vertex coordinate by angle in the y direction about c
 void rotate_y(double angle, vertex_t *v, vertex_t c) {
-    double x1 = v->x;
+    double x1 = v->x - c.x;
     double z1 = v->z - c.z;
-    v->x = x1 * cos(angle) + z1 * sin(angle);
+    v->x = x1 * cos(angle) + z1 * sin(angle) + c.x;
     v->z = -x1 * sin(angle) + z1 * cos(angle) + c.z;
 }
 
 // rotate_z : rotate the vertex coordinate by angle in the z direction about c
 void rotate_z(double angle, vertex_t *v, vertex_t c) {
-    double x1 = v->x;
-    double y1 = v->y;
-    v->x = x1 * cos(angle) - y1 * sin(angle);
-    v->y = x1 * sin(angle) + y1 * cos(angle);
+    double x1 = v->x - c.x;
+    double y1 = v->y - c.y;
+    v->x = x1 * cos(angle) - y1 * sin(angle) + c.x;
+    v->y = x1 * sin(angle) + y1 * cos(angle) + c.y;
 }
