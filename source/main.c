411dd4dd571f00fe7a23907062a641de51ac6ba8
Anselmo Jimenez <anselmojimenez0@gmail.com>
2025-10-01 00:11:06 -0400

    testing scanline algorithm

--- source/main.c
+++ source/main.c
@@ -47,34 +47,44 @@ int main(int argc, char const *argv[]) {
     object_t obj = load(fp);
     vertex_t transforms[MAXVERTICES];
 
-    for (;;) {
-        switch (keypress()) {
-            case 'q':
-                destroy_window();
-                fclose(fp);
-                return 0;
-            default: break;
-        }
+    for (int i = 0; i < vcount; i++)
+        transforms[i] = obj.vertices[i];
 
-        clear_window();
-
-        rotation_angle += 0.005;
-
-        for (int i = 0; i < vcount; i++) {
-            transforms[i] = obj.vertices[i];
-            rotate_z(rotation_angle, &transforms[i]);
-            // rotate_y(rotation_angle, &transforms[i]);
-        }
-
-        for (int i = 0; i < fcount; i++) {
-            draw_surface(transforms[obj.faces[i].vertex_index[0] - 1], 
+    for (int i = 0; i < fcount; i++) {
+        draw_surface(transforms[obj.faces[i].vertex_index[0] - 1], 
                     transforms[obj.faces[i].vertex_index[1] - 1], 
                     transforms[obj.faces[i].vertex_index[2] - 1],
                     obj.normals[obj.faces[i].normal_index]);
-        }
-    
-        draw_window();
     }
+
+    // for (;;) {
+    //     switch (keypress()) {
+    //         case 'q':
+    //             destroy_window();
+    //             fclose(fp);
+    //             return 0;
+    //         default: break;
+    //     }
+
+    //     clear_window();
+
+    //     rotation_angle += 0.005;
+
+    //     for (int i = 0; i < vcount; i++) {
+    //         transforms[i] = obj.vertices[i];
+    //         rotate_z(rotation_angle, &transforms[i]);
+    //         // rotate_y(rotation_angle, &transforms[i]);
+    //     }
+
+    //     for (int i = 0; i < fcount; i++) {
+    //         draw_surface(transforms[obj.faces[i].vertex_index[0] - 1], 
+    //                 transforms[obj.faces[i].vertex_index[1] - 1], 
+    //                 transforms[obj.faces[i].vertex_index[2] - 1],
+    //                 obj.normals[obj.faces[i].normal_index]);
+    //     }
+    
+    //     draw_window();
+    // }
 
     // will never be reached but just for sanity's sake
     destroy_window();
