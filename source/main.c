b3990462ea8e926524d5b3ac54f51707f49936e6
Anselmo Jimenez <anselmojimenez0@gmail.com>
2025-10-26 22:08:38 -0400

    Added support for multiple options

--- source/main.c
+++ source/main.c
@@ -19,27 +19,53 @@ void print_faces(object_t obj) {
 #endif
 
 int main(int argc, char const *argv[]) {
-    const int height = 130;
-    const int width = 512;
+    int height = 128;
+    int width = 512;
+    int x_rot = 0;
+    int y_rot = 0;
+    int z_rot = 0;
     double rotation_angle = 0;
     FILE *fp;
 
-    int option = getopt(argc, argv, "hf:");
-    switch (option) {
-        case 'f':
-            if (optarg == NULL) {
+    int option = 0;
+    while ((option = getopt(argc, argv, "hf:d:xyz")) != -1) {
+        switch (option) {
+            case 'f':
+                if (optarg == NULL) {
+                    printf(USAGE);
+                    return 1;
+                }
+                else if ((fp = fopen(optarg, "r")) == NULL) {
+                    printf("LuminANSI: Unable to open file '%s'\n", optarg);
+                    return 1;
+                }
+                break;
+            case 'd':
+                int temp_height = 0;
+                int temp_width = 0;
+                if (optarg == NULL || sscanf(optarg, "%dx%d", &temp_height, &temp_width) != 2) {
+                    printf(USAGE);
+                    return 1;
+                }
+                if (temp_height < 0 || temp_width < 0) {
+                    printf("LuminANSI: Negative dimensions not allowed\n");
+                    return 1;
+                }
+                else if ((0 < temp_height && temp_height < 128) || (0 < temp_width && temp_width < 512)) {
+                    printf("LuminANSI: Screen defaulting to 128x512\n");
+                    break;
+                }
+                width = temp_width;
+                height = temp_height;
+                break;
+            case 'x': x_rot = 1; break;
+            case 'y': y_rot = 1; break;
+            case 'z': z_rot = 1; break;
+            case 'h':
+            default:
                 printf(USAGE);
                 return 1;
-            }
-            else if ((fp = fopen(optarg, "r")) == NULL) {
-                printf("asciipoly: Unable to open file '%s'\n", optarg);
-                return 1;
-            }
-            break;
-        case 'h':
-        default:
-            printf(USAGE);
-            return 1;
+        }
     }
     
     object_t obj = load(fp);
@@ -59,13 +85,13 @@ int main(int argc, char const *argv[]) {
 
         clear_window();
 
-        rotation_angle += 0.01;
+        rotation_angle += 0.005;
 
         for (int i = 0; i < vcount; i++) {
             transforms[i] = obj.vertices[i];
-            rotate_z(rotation_angle, &transforms[i]);
-            rotate_y(rotation_angle, &transforms[i]);
-            // rotate_x(rotation_angle, &transforms[i]);
+            if (x_rot) rotate_x(rotation_angle, &transforms[i]);
+            if (y_rot) rotate_y(rotation_angle, &transforms[i]);
+            if (z_rot) rotate_z(rotation_angle, &transforms[i]);
         }
 
         for (int i = 0; i < fcount; i++) {
